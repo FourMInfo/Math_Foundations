de656fcd67d4f3b6cb159ff46e857dc5596b94cd
Aron T <aront@fourm.info>
2024-10-19 16:04:40 +0300

    nth-root function

---
+++ src/basic_maths.jl
@@ -0,0 +1,15 @@
+"""
+    nth_root(x,n) → c
+Calculate nth root of x handling negative x properly
+"""
+function nth_root(x, n)
+    if x < 0
+        if n % 2 == 1
+            -abs(x)^(1/n)
+        else
+            Complex(x)^(1/n)
+        end
+    else
+        x^(1/n)
+    end
+end
