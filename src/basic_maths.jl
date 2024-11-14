60b41b226e871bba09536f7651f18d81de9e3f72
Aron T <aront@fourm.info>
2024-11-14 17:18:39 +0200

    change parabola w/roots

--- src/basic_maths.jl
+++ src/basic_maths.jl
@@ -2,7 +2,8 @@ using Symbolics
 using Plots; gr
 using LaTeXStrings
 using Dates
-using AMRVW
+# using AMRVW
+using Polynomials
 
 """
     nth_root(x,n) → c
@@ -21,9 +22,10 @@ function nth_root(x, n)
 end
 
 """
-    plot_parabola(a₂::Float64, a₁::Float64=0.0, a₀::Float64=0.0)
+    plot_parabola_roots_1(a₂::Float64, a₁::Float64=0.0, a₀::Float64=0.0) -> [ComplexF64, ComplexF64]
+AMRVW version of plotting parabola with roots
 """
-function plot_parabola(a₂::Float64, a₁::Float64=0.0, a₀::Float64=0.0)
+function plot_parabola_roots_1(a₂::Float64, a₁::Float64=0.0, a₀::Float64=0.0)
     gr()
     @variables x
     f = a₂*x^2 + a₁*x + a₀
@@ -32,6 +34,21 @@ function plot_parabola(a₂::Float64, a₁::Float64=0.0, a₀::Float64=0.0)
     plot(f,legend=false,xlims=[-4,4],ylims=[-4,4],framestyle = :origin)
     title!(L"Plot\ of\ %$a₂ * x^2 + %$a₁ * x + %$a₀\\")
     scatter!(r3c,series_annotations = text.(r3, 8, :bottom))
-    savefig("plots/"* Dates.format(now(),"yyyymmdd-HHMMSS") * "parabola.png")
+    savefig("plots/"* Dates.format(now(),"yyyymmdd-HHMMSS") * "parabolaAMRVW.png")
     r3c
+end
+"""
+    plot_parabola_roots_2(a₂::Float64, a₁::Float64=0.0, a₀::Float64=0.0) -> [ComplexF64, ComplexF64]
+Polynomial version of plotting parabola with roots
+"""
+function plot_parabola_roots_2(a₂::Float64, a₁::Float64=0.0, a₀::Float64=0.0)
+    gr()
+    @variables x
+    f = a₂*x^2 + a₁*x + a₀
+    r3 = round.(roots(Polynomial([a₀,a₁,a₂], :x)), digits=12)
+    plot(f,legend=false,xlims=[-4,4],ylims=[-4,4],framestyle = :origin)
+    title!(L"Plot\ of\ %$a₂ * x^2 + %$a₁ * x + %$a₀\\")
+    scatter!(r3,series_annotations = text.(r3, 8, :bottom))
+    savefig("plots/"* Dates.format(now(),"yyyymmdd-HHMMSS") * "parabolaPoly.png")
+    r3
 end
